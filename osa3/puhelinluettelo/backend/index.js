03f444a0f846a1cf8dd2317eab7198bef6b027ef
Roberto Martinez Keisala <robomarti@outlook.com>
2023-07-06 19:12:49 +0300

    osa3 updates

--- osa3/puhelinluettelo/backend/index.js
+++ osa3/puhelinluettelo/backend/index.js
@@ -2,98 +2,101 @@ const express = require('express')
 const app = express()
 const morgan = require('morgan')
 const cors = require('cors')
+const mongoose = require('mongoose')
 
 app.use(express.json())
 app.use(morgan('tiny'))
 app.use(cors())
 app.use(express.static('build'))
 
-let persons = [
-    {
-      id: 1,
-      name: "Arto Hellas",
-      number: "040-123456"
-    },
-    {
-      id: 2,
-      name: "Ada Lovelace",
-      number: "39-44-5323523"
-    },
-    {
-      id: 3,
-      name: "Dan Abramov",
-      number: "12-43-234345"
-    },
-    {
-      id: 4,
-      name: "Mary Poppendick",
-      number: "39-23-6423122"
-    },
-  ]
+import { password } from './password_text.js'
 
-  app.get('/info', (request, response) => {
-    response.send(`<p>Phonebook has info for ${persons.length} people</p><p>${Date()}</p>`)
-  })
+const url = `mongodb+srv://fullstack:${password}@cluster0.qmqdw0l.mongodb.net/phonebookApp?retryWrites=true&w=majority`
+
+mongoose.set('strictQuery',false)
+mongoose.connect(url)
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+})
+
+personSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+app.get('/info', (request, response) => {
+  response.send(`<p>Phonebook has info for ${persons.length} people</p><p>${Date()}</p>`)
+})
   
-  app.get('/api/persons', (request, response) => {
+app.get('/api/persons', (request, response) => {
+  Person.find({})
+  .then(persons => {
     response.json(persons)
   })
+})
 
-  app.get('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const person = persons.find(person => person.id === id)
-    if (person) {
-      response.json(person)
-    } else {
-      response.status(404).end()
-    }
-  })
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const person = persons.find(person => person.id === id)
+  if (person) {
+    response.json(person)
+  } else {
+    response.status(404).end()
+  }
+})
 
-  const generateId = () => {
-    return Math.floor(Math.random() * 1000000000000)
+const generateId = () => {
+  return Math.floor(Math.random() * 1000000000000)
+}
+  
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+
+  if (!body.name) {
+    return response.status(400).json({ 
+      error: 'name missing' 
+    })
+  }
+
+  if (!body.number) {
+    return response.status(400).json({ 
+      error: 'number missing' 
+    })
+  }
+
+  const alreadyExists = persons.find(person => person.name === body.name)
+  if (alreadyExists) {
+    return response.status(400).json({ 
+      error: 'name is already in phonebook' 
+    })
   }
   
-  app.post('/api/persons', (request, response) => {
-    const body = request.body
+  const person = {
+    name: body.name,
+    number: body.number || false,
+    id: generateId(),
+  }
   
-    if (!body.name) {
-      return response.status(400).json({ 
-        error: 'name missing' 
-      })
-    }
-
-    if (!body.number) {
-      return response.status(400).json({ 
-        error: 'number missing' 
-      })
-    }
-
-    const alreadyExists = persons.find(person => person.name === body.name)
-    if (alreadyExists) {
-      return response.status(400).json({ 
-        error: 'name is already in phonebook' 
-      })
-    }
+  persons = persons.concat(person)
   
-    const person = {
-      name: body.name,
-      number: body.number || false,
-      id: generateId(),
-    }
-  
-    persons = persons.concat(person)
-  
-    response.json(person)
+  response.json(person)
   })
 
-  app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    persons = persons.filter(person => person.id !== id)
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  persons = persons.filter(person => person.id !== id)
+
+  response.status(204).end()
+})
   
-    response.status(204).end()
-  })
-  
-  const PORT = process.env.PORT || 3001
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
-  })
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
---
+++ osa3/puhelinluettelo/backend/password_text.js
@@ -0,0 +1,3 @@
+const password = "moi"
+
+export { password }
