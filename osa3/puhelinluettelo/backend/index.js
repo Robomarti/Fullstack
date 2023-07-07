14e3ad8992ef650a3f7e3126a6cff7452d297765
Roberto Martinez Keisala <robomarti@outlook.com>
2023-07-07 09:47:29 +0300

    uibuild

--- osa3/puhelinluettelo/backend/index.js
+++ osa3/puhelinluettelo/backend/index.js
@@ -6,15 +6,11 @@ require('dotenv').config()
 const Person = require('./models/person')
 
 const requestLogger = (request, response, next) => {
-  console.log('Method:', request.method)
-  console.log('Path:  ', request.path)
-  console.log('Body:  ', request.body)
-  console.log('---')
-  next()
-}
-
-const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'unknown endpoint' })
+	console.log('Method:', request.method)
+	console.log('Path:  ', request.path)
+	console.log('Body:  ', request.body)
+	console.log('---')
+	next()
 }
 
 app.use(cors())
@@ -22,65 +18,85 @@ app.use(express.json())
 app.use(requestLogger)
 app.use(express.static('build'))
 
-app.get('/info', (request, response) => {
-  response.send(`<p>Phonebook has info for ${persons.length} people</p><p>${Date()}</p>`)
-})
-  
 app.get('/api/persons', (request, response) => {
-  Person.find({})
-  .then(persons => {
-    response.json(persons)
-  })
+	Person.find({})
+		.then(persons => {
+			response.json(persons)
+		})
 })
 
-app.get('/api/persons/:id', (request, response) => {
-  Person.findById(request.params.id).then(person =>{
-    response.json(person)
-  })
+app.get('/api/persons/:id', (request, response, next) => {
+	Person.findById(request.params.id)
+		.then(person => {
+			if (person)
+			{
+				response.json(person)
+			}
+			else
+			{
+				response.status(404).end()
+			}
+		})
+		.catch(error => next(error))
 })
 
 const generateId = () => {
-  return Math.floor(Math.random() * 1000000000000)
+	return Math.floor(Math.random() * 1000000000000)
 }
-  
-app.post('/api/persons', (request, response) => {
-  const body = request.body
 
-  if (body.name === undefined) {
-    return response.status(400).json({ error: 'name missing' })
-  }
+app.post('/api/persons', (request, response, next) => {
+	const body = request.body
 
-  if (body === undefined) {
-    return response.status(400).json({ error: 'number missing' })
-  }
-
-  /*const alreadyExists = persons.find(person => person.name === body.name)
+	/*const alreadyExists = persons.find(person => person.name === body.name)
   if (alreadyExists) {
     return response.status(400).json({ error: 'name is already in phonebook' })
   }
   */
 
-  const person = new Person({
-    name: body.name,
-    number: body.number || false,
-    id: generateId(),
-  })
-  
-  person.save().then(savedPerson => {
-    response.json(savedPerson)
-  })  
+	const person = new Person({
+		name: body.name,
+		number: body.number,
+		id: generateId(),
+	})
+
+	person.save()
+		.then(savedPerson => {
+			response.json(savedPerson)
+		})
+		.catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  persons = persons.filter(person => person.id !== id)
-
-  response.status(204).end()
+app.delete('/api/persons/:id', (request, response, next) => {
+	Person.findByIdAndDelete(request.params.id)
+		.then(() => {
+			response.status(204).end()
+		})
+		.catch(error => next(error))
 })
+
+const unknownEndpoint = (request, response) => {
+	response.status(404).send({ error: 'unknown endpoint' })
+}
 
 app.use(unknownEndpoint)
-  
+
+const errorHandler = (error, request, response, next) => {
+	console.error(error.message)
+
+	if (error.name === 'CastError') {
+		return response.status(400).send({ error: 'malformatted id' })
+	}
+	else if (error.name === 'ValidationError') {
+		return response.status(400).json({ error: error.message })
+	}
+
+	next(error)
+}
+
+// tämä tulee kaikkien muiden middlewarejen rekisteröinnin jälkeen!
+app.use(errorHandler)
+
 const PORT = process.env.PORT
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+	console.log(`Server running on port ${PORT}`)
 })
